57e3b870baa31b196b24711f7a7235470cbef10e
weid2014 <26858826@qq.com>
2022-07-31 14:29:04 +0800

    创建项目

---
+++ app/src/main/java/com/linde/refrigeratormanagementsystem/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.linde.refrigeratormanagementsystem.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.linde.refrigeratormanagementsystem.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
