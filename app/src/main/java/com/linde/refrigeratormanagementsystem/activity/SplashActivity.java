b37ed0ce8d53e39579f321e3a8482c92d2f531cb
weid2014 <26858826@qq.com>
2022-07-31 14:55:34 +0800

    创建闪屏页

--- app/src/main/java/com/linde/refrigeratormanagementsystem/activity/SplashActivity.java
+++ app/src/main/java/com/linde/refrigeratormanagementsystem/activity/SplashActivity.java
@@ -2,8 +2,11 @@ package com.linde.refrigeratormanagementsystem.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.linde.refrigeratormanagementsystem.MainActivity;
 import com.linde.refrigeratormanagementsystem.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,5 +15,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        jumpToMain();
+    }
+
+    private void jumpToMain(){
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },1000);
+
     }
 }
